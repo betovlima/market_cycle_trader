ca3485e1e0ffeee4afa4b9b41d5c12b38c33808a
betovlima <betovlima@gmail.com>
2026-09-05 22:32:56 -0300

    fix: add strategy and operations navigation translations

---
+++ src/i18n/strategyOperationsTranslations.js
@@ -0,0 +1,7 @@
+export const PT_STRATEGY_OPERATIONS_TRANSLATIONS = {
+  'Strategies & Operations': 'Estratégias e Operação',
+}
+
+export const ES_STRATEGY_OPERATIONS_TRANSLATIONS = {
+  'Strategies & Operations': 'Estrategias y Operación',
+}
