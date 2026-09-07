594363a9da39ad87396c161817f502e29834b8c1
betovlima <betovlima@gmail.com>
2026-09-07 08:32:30 -0300

    chore: bump Front to v10.7.15

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
-export const FRONT_VERSION = '10.7.13'
+export const FRONT_VERSION = '10.7.15'
 
 const rawApiBaseUrl = import.meta.env.VITE_API_BASE_URL || ''
 export const API_BASE_URL = rawApiBaseUrl.replace(/\/$/, '')
